3d204f25a094f134856a41c753ee60646e114c78
Nathalia Campos <campossnathalia@gmail.com>
2020-01-13 09:31:42 -0300

    first commit: project using react

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+import './styles.css'; 
+
+const Header = () => <header id='main-header'>JSHunt</header>; 
+
+/*
+Código de cima é a mesma coisa que esse código:  
+class Header extends Component {
+    render() {
+        return <h1>Hello</h1>
+    }
+}
+*/
+
+export default Header; 
+
